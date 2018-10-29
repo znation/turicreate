2197a1b32656bc6662bdf97db628b7932ccc52b0
Zach Nation <znation@apple.com>
2018-10-29 13:29:04 -0700

    Clean up logging and thread handling

--- src/unity/lib/visualization/server.cpp
+++ src/unity/lib/visualization/server.cpp
@@ -5,7 +5,6 @@
  */
 
 #include <unity/lib/visualization/server.hpp>
-#include <unity/lib/visualization/thread.hpp>
 
 #include <boost/lexical_cast.hpp>
 #include <boost/network/protocol/http/server.hpp>
@@ -23,7 +22,7 @@ typedef http::server<handler_type> http_server;
 
 static int find_port() {
     for (int port=8000; port<=9000; port++) {
-        std::cerr << "DEBUG: checking port " << port << std::endl;
+        logstream(LOG_DEBUG) << "WebServer: checking port " << port << std::endl;
         int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
         if (sock < 0) {
             if (errno == EMFILE) {
@@ -42,7 +41,7 @@ static int find_port() {
         if (error == 0) {
             return port;
         } else {
-            std::cerr << "DEBUG: return was " << error << ", error was " << errno << " (" << std::strerror(errno) << ")" << std::endl;
+            logstream(LOG_DEBUG) << "WebServer: return was " << error << ", error was " << errno << " (" << std::strerror(errno) << ")" << std::endl;
         }
 
     }
@@ -54,20 +53,19 @@ public:
     void operator() (http_server::request const &request,
                      http_server::connection_ptr const &connection) {
         static http_server::response_header headers[] = {{"Connection", "close"},
-                                                    {"Content-Type", "text/plain"}};
-        std::cerr << "DEBUG: handling connection\n";
+                                                         {"Content-Type", "text/plain"}};
         connection->set_status(http_server::connection::status_t::ok);
         connection->set_headers(boost::make_iterator_range(headers, headers + 2));
         std::string msg = "Hello, World!";
         connection->write(msg, [](std::error_code ec) {
-            std::cerr << "DEBUG: finished writing to connection object. Error code was " << ec << std::endl;
+            if (ec) {
+                logstream(LOG_ERROR) << "WebServer: failed writing to connection object. Error code was " << ec << std::endl;
+            }
         });
-        std::cerr << "DEBUG: returning from handling connection\n";
     }
 
     void log(http_server::string_type const &info) {
-        // TODO use turi logging
-        std::cerr << "ERROR: " << info << '\n';
+        logstream(LOG_ERROR) << "WebServer: " << info << '\n';
     }
 };
 
@@ -78,33 +76,36 @@ public:
     std::unique_ptr<http_server> m_server = nullptr;
     std::string m_port;
 
+    // single background thread for server to run on and listen (it then hands off requests to a thread pool)
+    std::thread m_thread;
+
     Impl() : m_handler(), m_options(m_handler) {
-        std::cerr << "DEBUG: starting WebServer::Impl\n";
+        logstream(LOG_DEBUG) << "WebServer: starting WebServer::Impl\n";
         m_options.thread_pool(
             std::make_shared<boost::network::utils::thread_pool>());
         m_port = std::to_string(find_port());
         m_server.reset(new http_server(m_options.address("localhost").port(m_port)));
-        run_thread([this]() {
-            std::cerr << "DEBUG: server `run` starting\n";
+        m_thread = std::thread([this]() {
             m_server->run();
-            std::cerr << "DEBUG: server `run` completed\n";
         });
-        std::cerr << "DEBUG: finished starting WebServer::Impl\n";
+        logstream(LOG_DEBUG) << "WebServer: finished starting WebServer::Impl\n";
     }
     ~Impl() {
-        std::cerr << "DEBUG: destroying WebServer::Impl\n";
+        logstream(LOG_DEBUG) << "WebServer: destroying WebServer::Impl\n";
+        m_server->stop();
+        m_thread.join();
     }
 };
 
-WebServer::WebServer() : m_impl(new Impl()) {}
+WebServer::WebServer() : m_impl(new Impl()) {
+    logstream(LOG_DEBUG) << "WebServer: starting WebServer\n";
+}
 WebServer::~WebServer() {
-    std::cerr << "DEBUG: destroying WebServer\n";
+    logstream(LOG_DEBUG) << "WebServer: destroying WebServer\n";
 }
 
 std::string WebServer::get_url_for_plot(const Plot& plot) {
-    std::cerr << "DEBUG: getting url for plot\n";
     static WebServer server;
-    std::cerr << "DEBUG: past WebServer constructor\n";
     return server.add_plot(plot);
 }
 
