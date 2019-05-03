4a943ec46118a10221ef0dfd3cc9c75e9b635c3c
Zach Nation <znation@apple.com>
2019-05-02 23:58:52 -0700

    Try to fix node.js build

--- deps/cmake/ExternalProjectNode.cmake
+++ deps/cmake/ExternalProjectNode.cmake
@@ -6,14 +6,21 @@ if(APPLE AND TC_BUILD_IOS)
   set(EXTRA_CONFIGURE_FLAGS --host=arm-apple-darwin)
 endif()
 
-ExternalProject_Add(ex_node
-  PREFIX ${CMAKE_SOURCE_DIR}/deps/build/node
-  URL ${CMAKE_SOURCE_DIR}/deps/src/node-v10.15.3/
-  INSTALL_DIR ${CMAKE_SOURCE_DIR}/deps/local
-  CONFIGURE_COMMAND env CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} "CFLAGS=-fPIC ${ARCH_FLAG} ${C_REAL_COMPILER_FLAGS}" <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> ${EXTRA_CONFIGURE_FLAGS}
-  INSTALL_COMMAND make -j4 install
-  BUILD_BYPRODUCTS ${CMAKE_SOURCE_DIR}/deps/local/bin/node
-  BUILD_IN_SOURCE 1)
+add_custom_target(ex_node_copy_source
+  COMMAND rsync -a ${CMAKE_SOURCE_DIR}/deps/src/node-v10.15.3/ ${CMAKE_SOURCE_DIR}/deps/build/node-v10.15.3/
+  COMMENT "Copying node.js source from deps/src to deps/build"
+  VERBATIM
+)
+
+add_custom_target(ex_node
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/deps/build/node-v10.15.3/
+  DEPENDS ex_node_copy_source
+  COMMAND env CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} "CFLAGS=-fPIC ${ARCH_FLAG} ${C_REAL_COMPILER_FLAGS}" ./configure --prefix=${CMAKE_SOURCE_DIR}/deps/local ${EXTRA_CONFIGURE_FLAGS}
+  COMMAND make -j4
+  COMMAND make install
+  COMMENT "Building node.js from source"
+  VERBATIM
+)
 
 add_executable(node IMPORTED)
 set_property(TARGET node PROPERTY IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/deps/local/bin/node)
