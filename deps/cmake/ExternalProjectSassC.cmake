3754fe1db561dd2274b9eb26a49760e5e314ee6d
Zach Nation <znation@apple.com>
2019-05-05 14:09:30 -0700

    Get sassc incremental build working
    Turns out we don't need libsass

--- deps/cmake/ExternalProjectSassC.cmake
+++ deps/cmake/ExternalProjectSassC.cmake
@@ -3,11 +3,10 @@ ExternalProject_Add(ex_sassc
   URL ${CMAKE_SOURCE_DIR}/deps/src/sassc-3.5.0/
   INSTALL_DIR ${CMAKE_SOURCE_DIR}/deps/local
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND env CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} "CFLAGS=-fPIC ${ARCH_FLAG} ${C_REAL_COMPILER_FLAGS}" PREFIX=${CMAKE_SOURCE_DIR}/deps/local SASS_LIBSASS_PATH=${CMAKE_SOURCE_DIR}/deps/build/libsass/src/ex_libsass make -j4 -C ../ex_sassc
+  BUILD_COMMAND env CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} "CFLAGS=-fPIC ${ARCH_FLAG} ${C_REAL_COMPILER_FLAGS}" PREFIX=${CMAKE_SOURCE_DIR}/deps/local SASS_LIBSASS_PATH=${CMAKE_SOURCE_DIR}/deps/src/libsass-3.5.5/ make -j4 -C ../ex_sassc
   INSTALL_COMMAND cp ../ex_sassc/bin/sassc ${CMAKE_SOURCE_DIR}/deps/local/bin/
   BUILD_BYPRODUCTS ${CMAKE_SOURCE_DIR}/deps/local/bin/sassc
   BUILD_IN_SOURCE 0
-  DEPENDS ex_libsass
 )
 
 add_executable(sassc IMPORTED)
