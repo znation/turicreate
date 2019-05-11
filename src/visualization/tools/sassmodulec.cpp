0d8d26f14a74b7390e837435ade87bfda4824b17
Zach Nation <znation@apple.com>
2019-05-11 04:35:46 -0700

    WIP continued

--- src/visualization/tools/sassmodulec.cpp
+++ src/visualization/tools/sassmodulec.cpp
@@ -7,33 +7,34 @@
  * SassModuleC
  *
  * Like Sassc (sass/scss compiler), but with support for (S)CSS Modules.
- *
+ * 
  */
 
 #include <boost/program_options.hpp>
+#include <sass/context.h>
+
 #include <iostream>
 
 using namespace boost::program_options;
 
-int main(int argc, const char *argv[])
-{
+static int parse_command_line(int argc, const char *argv[], variables_map& vm_out) {
   options_description desc{"Options"};
   desc.add_options()
-    ("module,m", value<std::string>()->required(), "(S)CSS Module Source");
+    ("module,m", value<std::string>()->required(), "Specifies a path to an input (S)CSS Module")
+    ("out_dir,o", value<std::string>()->required(), "Specifies a path to an output directory");
 
   try
   {
-    variables_map vm;
-    store(parse_command_line(argc, argv, desc), vm);
-    notify(vm);
+    store(parse_command_line(argc, argv, desc), vm_out);
+    notify(vm_out);
 
-    if (vm.count("help")) {
+    if (vm_out.count("help")) {
       std::cout << desc << '\n';
       return 0;
     }
 
     std::cout << "Got it!" << '\n';
-    std::cout << vm["module"].as<std::string>() << '\n';
+    std::cout << vm_out["module"].as<std::string>() << '\n';
     return 0;
   }
   catch (const error &ex)
@@ -43,3 +44,25 @@ int main(int argc, const char *argv[])
     return 1;
   }
 }
+
+int main(int argc, const char *argv[])
+{
+  variables_map vm;
+  int ret = parse_command_line(argc, argv, vm);
+  if (ret != 0) return ret;
+
+  
+
+
+}
+
+Sass_File_Context* context = sass_make_file_context(vm["module"].as<std::string>());
+Sass_Compiler* compiler = sass_make_file_compiler(sass_context);
+sass_compiler_parse(compiler)
+sass_compiler_execute(compiler)
+std::string output = sass_context_get_output_string(context)
+// Retrieve errors during compilation
+error_status = sass_context_get_error_status(context)
+json_error = sass_context_get_error_json(context)
+// Release memory dedicated to the C compiler
+sass_delete_compiler(compiler)
