9e295c893ded79ab2929a11fe3cb10de34517fd6
Zach Nation <znation@apple.com>
2019-06-14 00:37:47 -0700

    Fix whitespace changes

--- src/capi/impl/capi_visualization.cpp
+++ src/capi/impl/capi_visualization.cpp
@@ -25,8 +25,8 @@ EXPORT tc_plot* tc_plot_create_1d(const tc_sarray* sa, const char* title,
                                   const char* x_axis_title,
                                   const char* y_axis_title,
                                   const tc_parameters*, tc_error** error) {
-    ERROR_HANDLE_START();
-    turi::ensure_server_initialized();
+  ERROR_HANDLE_START();
+  turi::ensure_server_initialized();
 
   CHECK_NOT_NULL(error, sa, "sarray", NULL);
 
@@ -35,7 +35,7 @@ EXPORT tc_plot* tc_plot_create_1d(const tc_sarray* sa, const char* title,
   return new_tc_plot(
       std::dynamic_pointer_cast<turi::visualization::Plot>(plot));
 
-    ERROR_HANDLE_END(error, NULL);
+  ERROR_HANDLE_END(error, NULL);
 }
 
 EXPORT tc_plot* tc_plot_create_2d(const tc_sarray* sa_x, const tc_sarray* sa_y,
@@ -60,50 +60,50 @@ EXPORT tc_plot* tc_plot_create_2d(const tc_sarray* sa_x, const tc_sarray* sa_y,
 EXPORT tc_plot* tc_plot_create_sframe_summary(const tc_sframe* sf,
                                               const tc_parameters* params,
                                               tc_error** error) {
-    ERROR_HANDLE_START();
-    turi::ensure_server_initialized();
+  ERROR_HANDLE_START();
+  turi::ensure_server_initialized();
 
-    CHECK_NOT_NULL(error, sf, "sframe", NULL);
+  CHECK_NOT_NULL(error, sf, "sframe", NULL);
 
-    std::shared_ptr<turi::model_base> plot = sf->value.plot();
-    return new_tc_plot(std::dynamic_pointer_cast<turi::visualization::Plot>(plot));
+  std::shared_ptr<turi::model_base> plot = sf->value.plot();
+  return new_tc_plot(std::dynamic_pointer_cast<turi::visualization::Plot>(plot));
     
-    ERROR_HANDLE_END(error, NULL);
+  ERROR_HANDLE_END(error, NULL);
 }
 
 EXPORT tc_flexible_type* tc_plot_get_vega_spec(const tc_plot* plot,
                                                tc_plot_variation variation,
                                                const tc_parameters*,
                                                tc_error** error) {
-    ERROR_HANDLE_START();
-    turi::ensure_server_initialized();
+  ERROR_HANDLE_START();
+  turi::ensure_server_initialized();
 
-    CHECK_NOT_NULL(error, plot, "plot", NULL);
+  CHECK_NOT_NULL(error, plot, "plot", NULL);
 
-    std::string vega_spec = plot->value->get_spec(variation);
-    return tc_ft_create_from_string(vega_spec.data(), vega_spec.size(), error);
+  std::string vega_spec = plot->value->get_spec(variation);
+  return tc_ft_create_from_string(vega_spec.data(), vega_spec.size(), error);
 
-    ERROR_HANDLE_END(error, NULL);
+  ERROR_HANDLE_END(error, NULL);
 }
 
 EXPORT tc_flexible_type* tc_plot_get_next_data(const tc_plot* plot,
                                                const tc_parameters*,
                                                tc_error** error) {
-    ERROR_HANDLE_START();
-    turi::ensure_server_initialized();
+  ERROR_HANDLE_START();
+  turi::ensure_server_initialized();
 
-    CHECK_NOT_NULL(error, plot, "plot", NULL);
+  CHECK_NOT_NULL(error, plot, "plot", NULL);
 
-    std::string vega_data = plot->value->get_next_data();
-    return tc_ft_create_from_string(vega_data.data(), vega_data.size(), error);
+  std::string vega_data = plot->value->get_next_data();
+  return tc_ft_create_from_string(vega_data.data(), vega_data.size(), error);
 
-    ERROR_HANDLE_END(error, NULL);
+  ERROR_HANDLE_END(error, NULL);
 }
 
 EXPORT bool tc_plot_finished_streaming(const tc_plot* plot,
                                        const tc_parameters*, tc_error** error) {
-    ERROR_HANDLE_START();
-    turi::ensure_server_initialized();
+  ERROR_HANDLE_START();
+  turi::ensure_server_initialized();
 
   CHECK_NOT_NULL(error, plot, "plot", NULL);
   return plot->value->finished_streaming();
@@ -112,15 +112,15 @@ EXPORT bool tc_plot_finished_streaming(const tc_plot* plot,
 }
 
 EXPORT tc_flexible_type* tc_plot_get_url(const tc_plot* plot, const tc_parameters* params, tc_error** error) {
-    ERROR_HANDLE_START();
-    turi::ensure_server_initialized();
+  ERROR_HANDLE_START();
+  turi::ensure_server_initialized();
 
-    CHECK_NOT_NULL(error, plot, "plot", NULL);
+  CHECK_NOT_NULL(error, plot, "plot", NULL);
 
-    std::string url = plot->value->get_url();
-    return tc_ft_create_from_string(url.data(), url.size(), error);
+  std::string url = plot->value->get_url();
+  return tc_ft_create_from_string(url.data(), url.size(), error);
 
-    ERROR_HANDLE_END(error, NULL);
+  ERROR_HANDLE_END(error, NULL);
 }
 
 #ifdef __APPLE__
